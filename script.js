3ad911e5ce578b4a156e9a8dbc9a9615dffa5aca
muhdsyahirk <muhdsyahirk@gmail.com>
2025-12-06 14:14:42 +0800

    Main page done (99%)

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ const observerMainImg = new IntersectionObserver(
 );
 
 observerMainImg.observe(mainImg);
+
+const memberTitle = document.querySelector(".member-title");
+
+const observerMemberTitle = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("member-title-show", entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+observerMemberTitle.observe(memberTitle);
+
+const ctfTitle = document.querySelector(".ctfs-title");
+
+const observerCtfTitle = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("ctfs-title-show", entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+observerCtfTitle.observe(ctfTitle);
